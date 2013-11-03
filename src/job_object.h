052bdf4ec7945a2b7b9b2262906ad00cdca42dc1
lordmulder <mulder2@gmx.de>
2013-11-03 18:35:17 +0100

    Added JobObject class.

---
+++ src/job_object.h
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////////////////////////////////////
+// LameXP - Audio Encoder Front-End
+// Copyright (C) 2004-2013 LoRd_MuldeR <MuldeR2@GMX.de>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version, but always including the *additional*
+// restrictions defined in the "License.txt" file.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// http://www.gnu.org/licenses/gpl-2.0.txt
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+class QProcess;
+
+class JobObject
+{
+public:
+	JobObject(void);
+	~JobObject(void);
+
+	bool addProcessToJob(const QProcess *proc);
+	bool terminateJob(unsigned int exitCode = -1);
+
+private:
+	void *m_hJobObject;
+};

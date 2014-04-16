868c5d6895f503e6d71a3090ddcca241079d9766
lordmulder <mulder2@gmx.de>
2014-04-16 20:33:25 +0200

    Updated x265 version number.

--- src/encoder_x265.cpp
+++ src/encoder_x265.cpp
@@ -32,7 +32,7 @@
 
 //x265 version info
 static const unsigned int VERSION_X265_MINIMUM_VER = 9;
-static const unsigned int VERSION_X265_MINIMUM_REV = 29;
+static const unsigned int VERSION_X265_MINIMUM_REV = 53;
 
 // ------------------------------------------------------------
 // Helper Macros

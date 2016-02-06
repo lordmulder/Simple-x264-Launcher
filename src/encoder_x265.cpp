2f3eababe2d3497a02172bffed8dfed12bca1804
lordmulder <mulder2@gmx.de>
2016-02-06 12:11:31 +0100

    Updated x265 to v1.9.

--- src/encoder_x265.cpp
+++ src/encoder_x265.cpp
@@ -37,8 +37,8 @@
 #include <QRegExp>
 
 //x265 version info
-static const unsigned int VERSION_X265_MINIMUM_VER =  18;
-static const unsigned int VERSION_X265_MINIMUM_REV = 212;
+static const unsigned int VERSION_X265_MINIMUM_VER = 19;
+static const unsigned int VERSION_X265_MINIMUM_REV =  0;
 
 // ------------------------------------------------------------
 // Helper Macros

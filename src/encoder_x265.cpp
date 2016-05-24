82f12a59a4a0c82209f87fe3dc4c50261ba4ae24
lordmulder <mulder2@gmx.de>
2016-05-24 19:59:13 +0200

    Bump x265 version.

--- src/encoder_x265.cpp
+++ src/encoder_x265.cpp
@@ -40,7 +40,7 @@
 
 //x265 version info
 static const unsigned int VERSION_X265_MINIMUM_VER =  19;
-static const unsigned int VERSION_X265_MINIMUM_REV = 167;
+static const unsigned int VERSION_X265_MINIMUM_REV = 183;
 
 // ------------------------------------------------------------
 // Helper Macros

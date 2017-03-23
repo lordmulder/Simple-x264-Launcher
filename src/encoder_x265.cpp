f213759b5358e4a8c7af2d3d6a56a3c610642aa5
lordmulder <mulder2@gmx.de>
2017-03-23 20:59:03 +0100

    Bump x265 version.

--- src/encoder_x265.cpp
+++ src/encoder_x265.cpp
@@ -40,8 +40,8 @@
 #include <QPair>
 
 //x265 version info
-static const unsigned int VERSION_X265_MINIMUM_VER = 22;
-static const unsigned int VERSION_X265_MINIMUM_REV = 15;
+static const unsigned int VERSION_X265_MINIMUM_VER = 23;
+static const unsigned int VERSION_X265_MINIMUM_REV = 17;
 
 // ------------------------------------------------------------
 // Helper Macros

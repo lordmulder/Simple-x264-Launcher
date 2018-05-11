c0b0fa4662e79fd1935454afa595b622891c3aca
lordmulder <mulder2@gmx.de>
2018-05-11 10:56:52 +0200

    Updated x265 and NVEncC to latest versions.

--- src/encoder_nvencc.cpp
+++ src/encoder_nvencc.cpp
@@ -40,7 +40,7 @@
 #include <QPair>
 
 //x265 version info
-static const unsigned int VERSION_NVENCC_MINIMUM_VER = 330;
+static const unsigned int VERSION_NVENCC_MINIMUM_VER = 401;
 
 // ------------------------------------------------------------
 // Helper Macros
--- src/encoder_x265.cpp
+++ src/encoder_x265.cpp
@@ -40,8 +40,8 @@
 #include <QPair>
 
 //x265 version info
-static const unsigned int VERSION_X265_MINIMUM_VER = 27;
-static const unsigned int VERSION_X265_MINIMUM_REV =  1;
+static const unsigned int VERSION_X265_MINIMUM_VER =  27;
+static const unsigned int VERSION_X265_MINIMUM_REV = 346;
 
 // ------------------------------------------------------------
 // Helper Macros

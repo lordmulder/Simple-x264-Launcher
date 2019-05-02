35f97f35761e5303ce02d30f193dff1fa1bd4323
lordmulder <mulder2@gmx.de>
2019-05-02 13:05:43 +0200

    Updated x265 minimum required version to 3.0+8.

--- src/encoder_x265.cpp
+++ src/encoder_x265.cpp
@@ -40,8 +40,8 @@
 #include <QPair>
 
 //x265 version info
-static const unsigned int VERSION_X265_MINIMUM_VER = 29;
-static const unsigned int VERSION_X265_MINIMUM_REV =  1;
+static const unsigned int VERSION_X265_MINIMUM_VER = 30;
+static const unsigned int VERSION_X265_MINIMUM_REV =  8;
 
 // ------------------------------------------------------------
 // Helper Macros

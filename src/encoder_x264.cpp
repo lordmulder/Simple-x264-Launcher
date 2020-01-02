ee7155642b93c6d8493aa0d52eb2b58b9490a3d9
lordmulder <mulder2@gmx.de>
2020-01-03 00:46:44 +0100

    Bump x264 minimum required version to API-#159 (r2991).

--- src/encoder_x264.cpp
+++ src/encoder_x264.cpp
@@ -40,8 +40,8 @@
 #include <QPair>
 
 //x264 version info
-static const unsigned int VERSION_X264_MINIMUM_REV = 2982;
-static const unsigned int VERSION_X264_CURRENT_API =  158;
+static const unsigned int VERSION_X264_MINIMUM_REV = 2991;
+static const unsigned int VERSION_X264_CURRENT_API =  159;
 
 // ------------------------------------------------------------
 // Helper Macros
